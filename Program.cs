04dc3420b755926bbf77a868881781d4d9c5f7a8
Petr Makeev <essential@inbox.ru>
2022-05-10 13:47:09 +0300

    Program code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив строк длина которых меньше или равна 3 символа.
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] spled = text.Split(new char[]{' ', ':','.',',', '!', '?'}, StringSplitOptions.RemoveEmptyEntries);
+            var res = from word in spled
+                      where word.Length <= 3
+                      select word;
+            if (res.Count() <= 0)
+            {
+                Console.WriteLine("no 3 length words");
+            }
+            else
+            {
+                foreach (string s in res)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
